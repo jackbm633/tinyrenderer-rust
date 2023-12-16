acb4c55e99f962b0e14bfe59d902d28b9551ba1f
Jack Beckitt-Marshall <jack@pictura.co.uk>
2023-12-16 19:08:27 +0000

    Initial simple image code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use image::{Rgb, RgbImage, imageops::flip_vertical_in_place};
+
+const WHITE: Rgb<u8> = Rgb([255,0,0]);
+const RED: Rgb<u8> = Rgb([255,0,0]);
+
 fn main() {
-    println!("Hello, world!");
+    let mut img = RgbImage::new(100, 100);
+
+    img.put_pixel(52, 41, RED);
+    flip_vertical_in_place(&mut img);
+    
+    match img.save("img.bmp") {
+        Ok(_) => (),
+        Err(why) => panic!("{:?}", why)
+    }
 }
